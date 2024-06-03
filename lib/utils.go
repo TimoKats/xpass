a7189c27234244b7864351c837c8ec3409d676e6
TimoKats <tpakats@gmail.com>
2024-06-03 20:59:15 +0200

    more formatting details

--- lib/utils.go
+++ lib/utils.go
@@ -11,16 +11,16 @@ import(
 var Cyan = "\033[36m" 
 
 func InsecureInput(message string) string {
-	in := bufio.NewReader(os.Stdin)
+  in := bufio.NewReader(os.Stdin)
   fmt.Print(Cyan + "ACTION:   " + Reset + message)
-	command, _ := in.ReadString('\n')
-	return string(command)
+  command, _ := in.ReadString('\n')
+  return string(command)
 }
 
 func SecureInput(message string) string {
   fmt.Print(Cyan + "ACTION:   " + Reset + message)
   bytepw, _ := term.ReadPassword(int(syscall.Stdin))
-	fmt.Print("\n")
+  fmt.Print("\n")
   return string(bytepw)
 }
 
